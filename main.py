141ff3c9ffde46a2ef61394b9258816bfaa192ef
rbbozkurt <resit.b.bozkurt@campus.tu-berlin.de>
2022-10-21 22:10:24 +0200

    "init"

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import csv
+
 import gym
 import os
 from gym.wrappers import FlattenObservation
@@ -9,17 +11,18 @@ from stable_baselines3 import PPO, A2C
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
-
 import ware_house.envs
+
+LOG_PATH = ""
 
 
 def test_gym(environment):
-    episodes = 5
+    episodes = 1
     for episode in range(1, episodes + 1):
         state, info = environment.reset()
         done = False
         score = 0
-       # print(info)
+        # print(info)
         while not done:
             action = environment.action_space.sample()
             n_state, reward, done, info = environment.step(action)
@@ -29,18 +32,27 @@ def test_gym(environment):
     environment.close()
 
 
-def train(environment):
-    log_path = os.path.join('Training', 'Logs')
-    model = A2C("MultiInputPolicy", environment, verbose=1)
-    model.learn(total_timesteps=400000)
-    model.save('a2c-rware')
-    evaluate_policy(model, environment, n_eval_episodes=10, render=True)
+def train(environment, learning_rate, time_step):
+    model = A2C("MultiInputPolicy", environment, learning_rate=learning_rate, verbose=1)
+    model.learn(total_timesteps=time_step)
+    # evaluate_policy(model, environment, n_eval_episodes=10, render=True)
 
 
-# Press the green button in the gutter to run the script.
+env_versions = ["1", "2", "3", "4"]
+learning_rates = [0.05, 0.1, 0.5, 0.7]
+time_steps = [50, 100, 1000, 5000]
+
 if __name__ == '__main__':
-    env = gym.make("rware-v1")
-    #test_gym(env)
-    train(env)
+
+    for env_version in env_versions:
+        for learning_rate in learning_rates:
+            for time_step in time_steps:
+                with open('warehouse.txt', 'a', encoding='UTF8', newline='') as f:
+                    f.write(f"Version number : {env_version}")
+                    f.write(f"Learning rate : {learning_rate}")
+                    f.write(f"Time steps : {time_step}")
+                    f.close()
+                env = gym.make(f"rware-v{env_version}")
+                train(env, learning_rate=learning_rate, time_step=time_step)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
